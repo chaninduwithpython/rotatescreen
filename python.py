afea5c16e3b31f1bac046180edd660ecfcf6bff9
chaninduwithpython <92987217+chaninduwithpython@users.noreply.github.com>
2021-10-22 17:52:03 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
+import rotatescreen
+import keyboard
 
+screen = rotatescreen.get_primary_display()
+
+keyboard.add_hotkey('ctrl+windows+up', screen.set_landscape, suppress=True)
+keyboard.add_hotkey('ctrl+windows+down', screen.set_portrait_flipped, suppress=True)
+keyboard.add_hotkey('ctrl+windows+right', screen.set_portrait_flipped(), suppress=True)
+keyboard.add_hotkey('ctrl+windows+left', screen.set_portrait, suppress=True)
+
+keyboard.wait()
